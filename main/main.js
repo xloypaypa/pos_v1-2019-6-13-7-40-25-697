cca3a5bf3b11a18f746e0b84d05621754cefbc21
xsu <xsu@thoughtworks.com>
2019-06-21 14:12:18 +0800

    can count items by default format.

--- main/main.js
+++ main/main.js
@@ -2,7 +2,26 @@
 
 function printReceipt(tags) {
     const itemMap = getAllItemMap();
-    console.log(itemMap)
+    const itemCount = getItemCount(tags);
+    console.log(itemCount);
+}
+
+function getItemCount(tags) {
+    const result = [];
+    for (let i = 0; i < tags.length; i++) {
+        let exist = false;
+        for (let j = 0; j < result.length; j++) {
+            if (result[j].key === tags[i]) {
+                result[j].count++;
+                exist = true;
+                break;
+            }
+        }
+        if (!exist) {
+            result.push({key: tags[i], count: 1});
+        }
+    }
+    return result;
 }
 
 function getAllItemMap() {
